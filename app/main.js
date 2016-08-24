e6ddb58b0f6a6eb37e0bdf8d62bd15abc3c5c2ee
test5d <test5d@ap-pc.local>
2016-08-25 02:51:42 +0800

    redux first

--- app/main.js
+++ app/main.js
@@ -2,6 +2,7 @@
 var greeter = require('./Greeter.js');
 document.getElementById('root').appendChild(greeter());
 */
+import { createStore } from 'redux';
 import React from 'react';
 import {render} from 'react-dom';
 import Greeter from './Greeter';
@@ -12,4 +13,26 @@ import './main.css';
 /*
 render(<Greeter salutation='您好@_@!' />, document.getElementById('root'));
 */
+/*
 render(<Layout />, document.getElementById('root'));
+*/
+
+const reducer = function (state, action) {
+	if(action.type === "INC"){
+		return state + action.payload;
+	}
+	if(action.type === "DEC"){
+		return state - action.payload;
+	}
+	return state;
+}
+
+const store = createStore(reducer, 0);
+
+store.subscribe(() => {
+	console.log("store changed", store.getState());
+});
+
+store.dispatch({type: "INC", payload: 1});
+store.dispatch({type: "INC", payload: 1});
+store.dispatch({type: "DEC", payload: 1});
